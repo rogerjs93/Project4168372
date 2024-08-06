2bf0246da04496dde5aa45392c2d446119d75597
Roger Salas <rogerjs93@gmail.com>
2024-08-06 20:31:50 +0300

    w

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import styled, { ThemeProvider } from 'styled-components';
 import { Header, Footer, Sidebar } from './components';
 import { Home, About, Services, Contact } from './pages';
@@ -31,12 +31,12 @@ function App() {
           <MainContent>
             <Sidebar />
             <ContentArea>
-              <Switch>
-                <Route exact path="/" component={Home} />
-                <Route path="/about" component={About} />
-                <Route path="/services" component={Services} />
-                <Route path="/contact" component={Contact} />
-              </Switch>
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/services" element={<Services />} />
+                <Route path="/contact" element={<Contact />} />
+              </Routes>
             </ContentArea>
           </MainContent>
           <Footer />
---
+++ src/pages/index.js
@@ -0,0 +1,4 @@
+export { Home } from './Home';
+export { About } from './About';
+export { Services } from './Services';
+export { Contact } from './Contact';
