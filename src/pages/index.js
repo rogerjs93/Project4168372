fc873d6747386b432e7157ddc5413550026f08a9
Roger Salas <rogerjs93@gmail.com>
2024-08-08 17:01:08 +0300

    a

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,5 +3,6 @@ export { About } from './About';
 export { Services } from './Services';
 export { Contact } from './Contact';
 export { default as Register } from '../components/Register';
-// If you have a Login component in the pages directory, uncomment the next line
-// export { Login } from './Login';
+export { Games } from './Games';
+export { Community } from './Community';
+export { Support } from './Support';
